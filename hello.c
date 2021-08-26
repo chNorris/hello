e777440ad007cbea7e082f4347924a71efb85f0d
Christian <cnorris5@huskers.unl.edu>
2021-08-26 16:17:14 +0000

    inital commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author:Christian Norris
+ *         cnorris@cse.unl.edu
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Christian Norris\n");
+
+  return 0;
+}
